c27e65e82dd4c868752a92415d04055bf8852719
Hitcat <lu15ange7@gmail.com>
2021-06-15 01:26:26 -0500

    Add files via upload

---
+++ PARBLOA610PRO_BUTTONS.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# 4 TOP BUTTONS
+xsetwacom set "10 inch PenTablet Keyboard pad" "Button" "1" "key meta space"
+xsetwacom set "10 inch PenTablet Keyboard pad" "Button" "2" "key alt "
+xsetwacom set "10 inch PenTablet Keyboard pad" "Button" "3" "key space "
+xsetwacom set "10 inch PenTablet Keyboard pad" "Button" "8" "key tab "
+#
+# WHEEL CENTER
+xsetwacom set "10 inch PenTablet Keyboard pad" "Button" "13" "key ctrl - "
+xsetwacom set "10 inch PenTablet Keyboard pad" "Button" "14" "key ctrl + "
+#
+# 4 BOTTON BUTTON
+xsetwacom set "10 inch PenTablet Keyboard pad" "Button" "9" "key ctrl z "
+xsetwacom set "10 inch PenTablet Keyboard pad" "Button" "10" "key ctrl y "
+xsetwacom set "10 inch PenTablet Keyboard pad" "Button" "11" "key b "
+xsetwacom set "10 inch PenTablet Keyboard pad" "Button" "12" "key e "
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp 99-A610PRO.hwdb /etc/udev/hwdb.d/
+cp A610PRO.tablet /usr/share/libwacom/
+cp 50-A610PRO.conf /etc/X11/xorg.conf.d/
